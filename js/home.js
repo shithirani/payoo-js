746de13d1d4fa8729cc49e513e7a4bb5352109f8
shithi <shithisarker20@gmail.com>
2024-09-19 20:04:38 +0600

    added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,39 @@
-console.log('oh')
+// step-1
+
+document.getElementById('add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('done');
+
+    // step-2
+
+   const addMoney= document.getElementById('input-add-money').value;
+ 
+   
+   const inputPin= document.getElementById('input-pin').value;
+   console.log(addMoney,inputPin);
+//    step-3
+
+if(inputPin === '1234'){
+    console.log('adding money');
+
+    // step-4
+const balence=document.getElementById('current-balence').innerText;
+console.log(typeof balence);
+
+// step-5
+
+const addMoneyinput=parseFloat(addMoney);
+const balenceNum=parseFloat(balence);
+const newBalence= addMoneyinput+balenceNum;
+console.log(newBalence);
+
+//  step-6
+document.getElementById('current-balence').innerText=newBalence
+}
+
+
+else{
+    alert('wrong,try again');
+}
+
+})
