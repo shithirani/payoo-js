4df1565493d29594f408cc8d340ad4f6413d5cf0
shithi <shithisarker20@gmail.com>
2024-09-19 22:15:30 +0600

    done

---
+++ js/cashOut.js
@@ -0,0 +1,22 @@
+document.getElementById('cash-out-money').addEventListener('click',function(event){
+    event.preventDefault();
+    
+    
+    const inputMoney=document.getElementById('input-cash-out-money').value;
+    const cashOutPin=document.getElementById('input-cash-out-pin').value;
+  
+    if(cashOutPin === '4545'){
+        const balence=document.getElementById('current-balence').innerText;
+        const newBalence=parseFloat(balence);
+        const cashOut=parseFloat(inputMoney);
+
+        const finalAmount=newBalence-cashOut;
+        console.log(finalAmount);
+
+        document.getElementById('current-balence').innerText=finalAmount;
+
+    }
+    else{
+        alert('wrong');
+    }
+})
---
+++ js/practice.js
@@ -0,0 +1,17 @@
+document.getElementById('add-money'),addEventListener('click',function(event){
+    event.preventDefault();
+  
+    const inputMoney=document.getElementById('input-add-money').value;
+    const inputPin=document.getElementById('input-pin').value;
+    
+    if(inputPin === '123'){
+        const balence=document.getElementById('current-balence').innerText;
+        const newBalence=parseFloat(balence);
+        const monyes=parseFloat(inputMoney);
+        const newAmmount=newBalence+monyes;
+        console.log(newAmmount);
+
+        document.getElementById('current-balence').innerText=newAmmount;
+
+    }
+})
